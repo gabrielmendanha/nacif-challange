a7abc99eca221352e066f658ff28af7ef76b823b
Gabriel Mendanha <gabrielmendanha@icloud.com>
2025-07-10 20:31:12 -0300

    feat: create CRUD operations on Todo entity

--- back/models.py
+++ back/models.py
@@ -4,3 +4,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), unique=True, nullable=False)
+    todos = db.relationship('Todo')
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
--- back/routes.py
+++ back/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
-from .models import User
+from .models import User, Todo
+from .extensions import db
 from werkzeug.security import check_password_hash
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
@@ -19,3 +20,51 @@ def login():
 
     access_token = create_access_token(identity=f'{user.id}')
     return jsonify(access_token=access_token)
+
+@main.route('/todos', methods=['GET'])
+@jwt_required()
+def list_todos():
+    user_id = get_jwt_identity()
+    todos = Todo.query.filter_by(user_id=user_id).all()
+    return jsonify([{'id': t.id, 'title': t.title, 'completed': t.completed} for t in todos]), 200
+
+@main.route('/todos', methods=['POST'])
+@jwt_required()
+def create_todo():
+    user_id = get_jwt_identity()
+    data = request.get_json()
+
+    if ('title' not in data) or (not data['title']):
+        return jsonify(message='Missing or empty title'), 400
+
+    todo = Todo(title=data['title'], completed=data.get('completed', False if 'completed' not in data else data['completed']), user_id=user_id)
+
+    db.session.add(todo)
+    db.session.commit()
+
+    return jsonify(id=todo.id, title=todo.title, completed=todo.completed), 201
+
+@main.route('/todos/<int:todo_id>', methods=['PUT'])
+@jwt_required()
+def update_todo(todo_id):
+    user_id = get_jwt_identity()
+    todo = Todo.query.filter_by(id=todo_id, user_id=user_id).first_or_404()
+    data = request.get_json()
+    todo.title = data.get('title', todo.title)
+    todo.completed = data.get('completed', todo.completed)
+    db.session.commit()
+    return jsonify(message='Updated successfully')
+
+@main.route('/todos/<int:todo_id>', methods=['DELETE'])
+@jwt_required()
+def delete_todo(todo_id):
+    user_id = get_jwt_identity()
+    todo = Todo.query.filter_by(id=todo_id, user_id=user_id).first()
+
+    if not todo:
+        return jsonify(message='Todo does not exist'), 400
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return jsonify(message='Deleted successfully'), 204
